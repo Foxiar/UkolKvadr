1b5aed3670cb491005c972743bc6ae8bc1ed1dfc
Foxiar <75813110+Foxiar@users.noreply.github.com>
2023-04-11 12:28:15 +0200

    Add files via upload

---
+++ ÚkolFryč/ÚkolFryč/Program.cs
@@ -0,0 +1,8 @@
+﻿using ÚkolFryč;
+
+Kvadr testObjektu = new Kvadr(5, 10, 15);
+Console.WriteLine(testObjektu.VypocetPovrchu());
+Console.WriteLine(testObjektu.VypocetObjemu());
+Console.WriteLine(testObjektu.VypocetStenoveUhlopricky());
+Console.WriteLine(testObjektu.VypocetTelesoveUhlopricky());
+Console.ReadKey();
---
+++ ÚkolFryč/ÚkolFryč/kvadr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÚkolFryč
+{
+    internal class Kvadr
+    {
+        public double StranaA { get; set; }
+        public double StranaB { get; set; }
+        public double StranaC { get; set; }
+
+        public Kvadr(double a, double b, double c) {
+            StranaA= a;
+            StranaB= b;
+            StranaC= c;
+        }
+        public double VypocetObjemu()
+        {
+            double vysledek = StranaA * StranaB * StranaC;
+            return vysledek;
+        }
+        public double VypocetPovrchu()
+        {
+            double vysledek = 2 * (StranaA*StranaB + StranaA*StranaC + StranaB*StranaC);
+            return vysledek;
+        }
+        public double VypocetTelesoveUhlopricky()
+        {
+            double vysledek = Math.Sqrt(Math.Pow(StranaA, 2) + Math.Pow(StranaB, 2) + Math.Pow(StranaC, 2));
+            return vysledek;
+        }
+        public double VypocetStenoveUhlopricky()
+        {
+            double vysledek = Math.Sqrt(Math.Pow(StranaA, 2) + Math.Pow(StranaB, 2));
+            return vysledek;
+        }
+    }
+}
